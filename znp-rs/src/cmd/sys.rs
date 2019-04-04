702454e2d6f2678777c20a23767f6a7c14d213c2
qm3ster <qm3ster@gmail.com>
2019-04-05 01:32:13 +0300

    Add SYS_RESET_IND

--- znp-rs/src/cmd/sys.rs
+++ znp-rs/src/cmd/sys.rs
@@ -3,6 +3,7 @@ use crate::areq::AreqIn;
 use crate::sreq::Sreq;
 use crate::znp_codec::{Subsys, ZpiCmd};
 use serde::{Deserialize, Serialize};
+use serde_repr::{Deserialize_repr, Serialize_repr};
 
 /// SYS_OSAL_START_TIMER
 #[derive(Serialize, Deserialize, Debug)]
@@ -23,6 +24,34 @@ impl Sreq for StartTimer {
     const MAX_SIZE: usize = 3;
 }
 
+#[derive(Serialize_repr, Deserialize_repr, PartialEq, Debug)]
+#[repr(u8)]
+pub enum ResetReason {
+    PowerUp = 0x00,
+    External = 0x01,
+    Watchdog = 0x02,
+}
+/// SYS_RESET_IND
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Reset {
+    /// Reason for the reset
+    pub reason: ResetReason,
+    /// Transport protocol revision
+    pub transport_rev: u8,
+    pub product_id: u8,
+    /// Major release number
+    pub major_rel: u8,
+    /// Minor release number
+    pub minor_rel: u8,
+    /// Hardware revision number
+    pub hw_rev: u8,
+}
+impl AreqIn for Reset {
+    const SUBSYS: Subsys = Subsys::SYS;
+    const CMD_ID: u8 = 0x80;
+}
+
+/// SYS_OSAL_TIMER_EXPIRED
 #[derive(Serialize, Deserialize, Debug)]
 pub struct TimerExpired {
     pub timer_id: u8,
@@ -55,11 +84,13 @@ impl Sreq for NvRead {
 
 #[derive(Debug)]
 pub enum In {
+    Reset(Reset),
     TimerExpired(TimerExpired),
 }
 impl In {
     pub fn from_cmd(cmd: ZpiCmd) -> Result<Self> {
         match cmd.cmd_id() {
+            Reset::CMD_ID => Ok(In::Reset(cmd.parse()?)),
             TimerExpired::CMD_ID => Ok(In::TimerExpired(cmd.parse()?)),
             _ => Err(Error::unimplemented(&cmd)),
         }
